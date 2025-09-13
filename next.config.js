2c4f1780d7efe011c1b410f2e40680cec79a0858
Oscar Chinchilla <oechinchilla@gmail.com>
2025-09-13 14:43:55 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
   basePath: isProd ? '/SEFA-ACEROS' : '',
   assetPrefix: isProd ? '/SEFA-ACEROS/' : '',
-  trailingSlash: true
+  images: { unoptimized: true },
+  trailingSlash: true,
 };
+
+module.exports = nextConfig;
