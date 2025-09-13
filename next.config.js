41ad37411fd71730f571e7164cafd23dc4a8c5fd
Oscar Chinchilla <oechinchilla@gmail.com>
2025-09-13 19:13:04 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,15 @@
 const isProd = process.env.NODE_ENV === 'production';
 const repo = 'SEFA-ACEROS';
 
-module.exports = {
-  output: 'export',              // reemplaza a `next export`
-  trailingSlash: true,           // ayuda en GitHub Pages
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   basePath: isProd ? `/${repo}` : '',
   assetPrefix: isProd ? `/${repo}/` : '',
-  images: { unoptimized: true }, // necesario en export estático
+  images: { unoptimized: true },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : ''
+  }
 };
+
+export default nextConfig;
